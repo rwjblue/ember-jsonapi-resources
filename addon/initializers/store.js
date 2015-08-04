0e7b9b0907b113930147c09c3fd726cf35ae918a
Robert Jackson <me@rwjblue.com>
2015-08-04 10:01:01 -0400

    Remove extra registration.
    Because the `service:store` is already available (since it is exported at `app/services/store.js` it will be auto-resolved without issue), you do not need to manually register it.

--- addon/initializers/store.js
+++ addon/initializers/store.js
@@ -13,7 +13,7 @@ import StoreService from 'ember-jsonapi-resources/services/store';
 */
 export function initialize(container, application) {
   const store = 'service:store';
-  application.register(store, StoreService);
+
   application.inject('route', 'store', store);
   application.inject('controller', 'store', store);
 }
